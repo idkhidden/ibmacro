af3350400c852f071dc2f2d771cddde814d3a2ba
idkhidden <91305428+idkhidden@users.noreply.github.com>
2025-05-16 19:00:03 +0500

    yeah

---
+++ ibmacro/brainrot.h
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#define Tralalero_Tralala cout
+#define Bombardiro_Crocodilo cin
+
+#define Lirili_Larila string
+#define Brr_Brr_Patapim int
+#define Chimpanzini_Bananstd bool
+#define Frigo_Camelo float
+#define La_Vaca_Saturno_Saturnita double
+#define Ballerina_Cappucina char
+#define Trulimero_Trulicina void
+#define Matteooooooooooooo auto
+
+#define Tung_Tung_Tung_Tung_Tung_Tung_Sahur return
+#define Capuccino_Assassino exit
+
+#define Bombombini_Gusini for
+#define Trippi_Troppi while
+#define Girafa_Celestre if
+#define Boneca_Ambalabu else
+#define Ta_Ta_Ta_Ta_Ta_Ta_Ta_Ta_Ta_Ta_Ta_Sahur switch
+#define Brri_Brri_Bicus_Dicus_Bombicus case
+#define Tric_Trac_Baraboom default
+#define Bobrito_Bandito break
+#define Frulli_Frulla continue
+
+#define Orangutini_Ananasini namespace
+#define Pot_hotspot using
+
+#define Tigroligre_Frutonni true
+#define Ballerino_Lololo false
+
+
+#define Svinino_Bombondino typedef
---
+++ ibmacro/example.cpp
@@ -0,0 +1,13 @@
+#include"brainrot.h"
+
+Pot_hotspot Orangutini_Ananasini std;
+
+Brr_Brr_Patapim main()
+{
+	Lirili_Larila brainrot;
+	Bombardiro_Crocodilo >> brainrot;
+	Tralalero_Tralala << "rahhhhhhhhhhhhhh!";
+
+	Tung_Tung_Tung_Tung_Tung_Tung_Sahur 0;
+
+}
